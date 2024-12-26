d57333397b398079fb5e3a7f132658bfc08572ac
RamiAbdelhamid <ramighassan10@gmail.com>
2024-12-26 19:58:47 +0300

    JavaScript Task

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# JavaScript-basic
+# Introdiction.
+
+---
+
+## This is "JavaScript-basic" task contains 6 Question.
+
+---
+
+## Question 1 contain 11 points Evaluation of Expressions.
+
+---
+
+### Question 2 to 6 Using prompt and alert: These methods are essential for collecting and displaying user input/output.
+
+---
+
+### This is live URL :
+
+https://ramiabdelhamid.github.io/JavaScript-basic/
---
+++ task.js
@@ -0,0 +1,121 @@
+
+        // 1
+        var x = -5
+        var y = 3
+        var z = x * y
+        console.log(z);
+
+        // 2
+        var j = 'javascript';
+        var t = 50;
+        console.log("javascript + ", t);
+
+        // 3
+        var w = 17;
+        var e = 5;
+        var mod = w % e
+        console.log(mod);
+
+        // 4
+        var mod2 = e % w;
+        console.log(mod2);
+
+        // 5
+        var r = 10;
+        var y = e / r;
+        console.log(y);
+
+        //  6
+        var u = 4;
+        var i = '4';
+        var o = u === i;
+        console.log(o);
+
+
+        // 7
+        var a = 4;
+        var s = a != e;
+        console.log(s);
+
+
+        // 8
+        var d = 7;
+        var f = 8;
+        var g = d <= f
+        console.log(g);
+
+
+
+        // 9
+        console.log("Hello" + 5)
+
+
+
+
+        // 10
+        let c = 10.7
+        console.log(Math.ceil(c))
+        console.log(Math.floor(c))
+
+
+
+        // 11
+        console.log(Math.pow(x, 2))
+
+
+
+let number = prompt ("what is the number ?")
+
+alert(number)
+
+
+
+
+
+
+// sum
+        let num1 = parseFloat(prompt("Enter the first number:"));
+
+        let num2 = parseFloat(prompt("Enter the second number:"));
+
+        let sum = num1 + num2;
+
+        console.log("The sum of the two numbers is: " + sum);
+        alert("The sum of the two numbers is: " + sum);
+
+
+
+
+
+//  product 
+        let num3 = parseFloat(prompt("Enter the first number:"));
+
+        let num4 = parseFloat(prompt("Enter the second number:"));
+
+        let product = num3 * num4;
+
+        console.log("The product of the two numbers is: " + product);
+        alert("The product of the two numbers is: " + product);
+
+
+
+
+// divide
+
+    let num5 = parseFloat(prompt("Enter the first number:"));
+
+    let num6 = parseFloat(prompt("Enter the second number:"));
+
+    let divide = num5/num6;
+
+    console.log("The divide of the two numbers is: " + divide);
+    alert("The divide of the two numbers is: " + divide);
+
+
+
+//  sum of two number
+ console.log ("The sum of two numbers");
+
+
+
+ 
